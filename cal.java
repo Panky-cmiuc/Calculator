17b2e67bf7348e51b87d181ca2795a72dbd569cf
Pankaj Boruah <pankajboruah28@gmail.com>
2019-10-02 20:54:06 +0530

    add function

--- cal.java
+++ cal.java
@@ -1,8 +1,24 @@
-class functions{
-
-}
+import java.util.Scanner;
 class cal{
 public static void main(String ar[]){
-
+functions ob=new functions(); 
+Scanner sc=new Scanner(System.in);
+System.out.println("1->add\n2->sub\n3->mul\n4->div");
+int x=sc.nextInt();
+System.out.println("Enter value 1:");
+int a=sc.nextInt();
+System.out.println("Enter value 2:");
+int b=sc.nextInt();
+if(x==1){
+ob.add(a, b);
 }
+
+
+sc.close();
+}
+}
+class functions{
+    void add(int x, int y){
+        System.out.println("addition = "+(x+y));
+    }
 }
